6f23103c29c5520b436e03131621dfd9e1f979c8
Lonnie VanZandt <lonnie.vanzandt@intercax.com>
2021-11-18 21:16:47 -0500

    share a pom

---
+++ src/main/java/com/intercax/App.java
@@ -0,0 +1,13 @@
+package com.intercax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
